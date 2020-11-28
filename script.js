a8ab23cd177f1fd526a8a570435b2159e9d92c05
Jose Jimenez <jrjimenez221@gmail.com>
2020-11-28 12:34:35 -0600

    Returning to assignment. Seperated script from html.

---
+++ script.js
@@ -0,0 +1,15 @@
+  //set global variables
+  let score = 0
+  let quizActive = false
+  console.log($("#q5a1").value)
+  console.log(5)
+  let $question1 = $("#question-1")
+  $('#question1').css('visibility', 'visible');
+  
+  //begin the quiz
+  function quiz() {
+    $("#start-button").on("click", function(){
+        let quizActive = true
+      
+    })
+  }
