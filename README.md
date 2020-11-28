6912e5fd016879916f025bc860a9bac752ae76f3
Jose Jimenez <jrjimenez221@gmail.com>
2020-11-28 13:21:52 -0600

    Had a headache causing typo but timer is up and running

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-This is the code quiz assignment given to us. Goal is to make a common quiz utilizing what we've learned so far.
-Things I should accomplish include:
--hooking a button to start the test
--which starts a timer I must set to drain and end the test when it runs out
--make the questions set to hidden, visible one by one as user progresses
--
+
+# Code Quiz
+
+### Summary 
+A simple quiz themed around pokemon.
+
+### Features
+* 60 second timer
+* Wrong answers deduct 5 seconds
+* Correct answers add to score
 
 
 
@@ -13,18 +17,3 @@ Things I should accomplish include:
 
 
 
-
-
-
-
-
-
-
-
-
-
-//personal notes
-I am actually starting this late, and with technical difficulties, have lost significant progress. 
-What I get for procrastinating and overestimating my abilities, none the less I'll try my best to make up for lost points.
-I like to work under pressure so i make a habit of procrastinating; it's when I'm most focused and it tends to work out usually. 
-This is not one of those times and I shall work to remedy this comprimising trait of mine.
--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-  //set global variables
-  let score = 0
-  let quizActive = false
-  console.log($("#q5a1").value)
-  console.log(5)
-  let $question1 = $("#question-1")
-  $('#question1').css('visibility', 'visible');
+//set global variables
+let score = 0
+let quizActive = false
+console.log("hello")
+console.log(5)
+let $question1 = $("#question-1")
+$('#question1').css('visibility', 'visible');
   
-  //begin the quiz
-  function quiz() {
-    $("#start-button").on("click", function(){
-        let quizActive = true
-      
-    })
-  }
+
+function timer() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+      $("#timer").text( timeLeft + " seconds remaining");
+      timeLeft--;
+      console.log(timeLeft)
+
+      if (timeLeft === 0) {
+        $("#timer").text("Time's Up");
+        //quizOver();
+        clearInterval(timeInterval);
+      }
+
+    }, 1000);
+}
+
+
+
+//begin the quiz
+function quiz() {
+    //timer()
+    console.log("fds")
+  //$("#start-button").on("click", function(){
+  //    let quizActive = true
+  //    $(".correctBtn").on("click", function(event){
+  //        $("#score").text(score += 10)
+  //    })
+  //    
+  //    
+  //  
+  //})
+}
+
+
+$("#startButton").on("click", function(event){
+    console.log("startquiz button working")
+    timer()
+    quiz()
+})
