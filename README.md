2a2fbeb135cd5ec9a808cfa33e95c8d0070aa482
Jose Jimenez <jrjimenez221@gmail.com>
2021-02-11 09:30:04 -0600

    On submit questions now loop, radio buttons cleared.

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ A simple quiz themed around pokemon.
 
 
 
+workflow
+*   basic html 
+    *   [x] jumbotron title
+    *   [ ] removed on start
+*   question array of 10 that each holds
+    *   [x] a question
+    *   [x] 4 choices
+    *   [x] a correct choice INT
+*   For loop
+    *   [x] button that proceeds to the next question to display
+    *   [ ] compares selected answer to the correct choice of each question
+*   timer
+    *   [x] counts down from 60secs
+    *   [x] on 0 end quiz and prompt for initials
--- script.js
+++ script.js
@@ -1,49 +1,75 @@
 //set global variables
-let score = 0
-let quizActive = false
-console.log("hello")
-console.log(5)
-let $question1 = $("#question-1")
-$('#question1').css('visibility', 'visible');
-  
+let score = 00;
+let quizActive = false;
+let timeLeft = 60;
+let questions = [
+  {
+    question: "What was Ash's first pokemon?",
+    answers: ["pikachu", "charmander", "squirtle", "tododile"],
+    correctA: 0,
+  },
+  {
+    question: "What type is Charmander?",
+    answers: ["Water", "Fire", "Dragon", "Fairy"],
+    correctA: 1,
+  },
+  {
+    question: "Which of the following is a 3rd gen pokemon",
+    answers: ["Entei", "Sudowoodo", "Sandshew", "Flygon"],
+    correctA: 3,
+  },
+];
+let currentQuestionIndex = 0
+
 
 function timer() {
-    var timeLeft = 60;
+  var timeInterval = setInterval(function () {
+    $("#timer").text(timeLeft + " seconds remaining");
+    timeLeft--;
+    console.log(timeLeft);
 
-    var timeInterval = setInterval(function() {
-      $("#timer").text( timeLeft + " seconds remaining");
-      timeLeft--;
-      console.log(timeLeft)
-
-      if (timeLeft === 0) {
-        $("#timer").text("Time's Up");
-        //quizOver();
-        clearInterval(timeInterval);
-      }
-
-    }, 1000);
+    if (timeLeft === 0) {
+      $("#timer").text("Time's Up");
+      quizOver();
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
-
-
+function quizOver() {
+  let initials = prompt("Enter your initials");
+  console.log(initials + ": " + score);
+}
 
 //begin the quiz
 function quiz() {
-    //timer()
-    console.log("fds")
-  //$("#start-button").on("click", function(){
-  //    let quizActive = true
-  //    $(".correctBtn").on("click", function(event){
-  //        $("#score").text(score += 10)
-  //    })
-  //    
-  //    
-  //  
-  //})
+  console.log("fds");
+  firstQuestion()
+}
+
+function firstQuestion() {
+  
+  const onQuestion = questions[currentQuestionIndex];
+  $("#question").text(onQuestion.question)
+  $("#firstChoice").text("1. " + onQuestion.answers[0])
+  $("#secondChoice").text("2. " + onQuestion.answers[1])
+  $("#thirdChoice").text("3. " + onQuestion.answers[2])
+  $("#fourthChoice").text("4. " + onQuestion.answers[3])
 }
 
 
-$("#startButton").on("click", function(event){
-    console.log("startquiz button working")
-    timer()
-    quiz()
+$("#answerBtn").on("click", function(event){
+  currentQuestionIndex = currentQuestionIndex + 1
+  // if (questions[currentQuestionIndex].correctA =  {
+
+  // })
+  document.querySelector("input[name='option']:checked").checked = false;
+  firstQuestion()
 })
+
+
+
+$("#startButton").on("click", function (event) {
+  timer();
+  quiz();
+});
+
